d1752413378ae00fb6b34cf7222051af11f3ceca
Nikhil golyan <44206091+Nikhil0509@users.noreply.github.com>
2020-01-22 18:27:59 +0530

    Create Number-of-1-Bits.c

---
+++ Bit Manipulation/Number-of-1-Bits.c
@@ -0,0 +1,14 @@
+//https://www.interviewbit.com/problems/number-of-1-bits/
+int numSetBits(unsigned int A) {
+    int count=0;
+    while(A!=0)
+    {
+        if((A&1)==1)
+        {
+            count++;
+            
+        }
+        A=A>>1;
+    }
+    return count;
+}
