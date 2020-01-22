5572d35e2acd372a53138823ea416f78b0ad4e04
Nikhil golyan <44206091+Nikhil0509@users.noreply.github.com>
2020-01-22 18:46:57 +0530

    Create Sort-By-Color.c

---
+++ Two Pointers/Sort-By-Color.c
@@ -0,0 +1,65 @@
+//https://www.interviewbit.com/problems/sort-by-color/
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i,j,k; 
+    int n1=m-l+1; 
+    int n2=r-m; 
+    int L[n1], R[n2]; 
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+    i = 0;j = 0;k = l; 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        int m = (l+r)/2; 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+        merge(arr, l, m, r); 
+    } 
+}  
+void sortColors(int* a, int n1) {
+   // int i=0,j=0,temp=0;
+  /*  for(i=0;i<n1;i++)
+    {
+        for(j=i+1;j<n1;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }*/
+    mergeSort(a,0,n1-1);
+}
